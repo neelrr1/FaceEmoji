6ba5dc0cacc80b92d4e4ba442139dccf178cb61d
Neel Raja <neelrr1@gmail.com>
2020-07-17 02:38:56 -0700

    It works!!! + added multer

--- index.js
+++ index.js
@@ -5,19 +5,32 @@ const port = 3000
 // Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
 
+// Local filesystem for testing
 const fs = require('fs');
 const img = fs.readFileSync('./face.jpg')
 // console.log(imgBase64)
+
+var multer  = require('multer')
+var upload = multer({storage: multer.memoryStorage()})
     
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 client.auth._getApplicationCredentialsFromFilePath("./auth.json")
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/form', express.static(__dirname + '/index.html'));
 
-app.post('/faceDetect', async (req, res) => {
+app.post('/faceDetect', upload.single('image'), async (req, res) => {
+
+    // TODO: File size limits and file validation?
+    // TODO: Consider switching to DiskStorage instead - higher load capability?
     
-    const [result] = await client.faceDetection(img);
+    if (!req.file) {
+        res.status(400).send('No files were uploaded.');
+        console.log(req.body)
+        return;
+    }
+        
+    const [result] = await client.faceDetection(req.file.buffer);
     const faces = result.faceAnnotations;
     
     const out = []
